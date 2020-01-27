47a73b01d7f0e251a6333f34cd4ccc0b7d7e3683
itaasa <itaasarvin@gmail.com>
2020-01-26 21:12:19 -0500

    Initial Commit

---
+++ SynthRPG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SynthRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
